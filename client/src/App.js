3f89b769b829a2a32c42d5d3bafdbf6811839f4a
AugustinSorel <sorelaugustin@gmail.com>
2022-01-03 18:28:03 +0100

    Header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import GlobalStyle from "./styles";
 import { ThemeProvider } from "styled-components";
 import theme from "./utils/theme";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
 
-      <h1>Hello world</h1>
+      <Header />
     </ThemeProvider>
   );
 }
---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div>
+      <h1>Gym Logger</h1>
+    </div>
+  );
+};
