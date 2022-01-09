adcfc6b58d7860a98b1b0d8376b7263544509ddb
AugustinSorel <sorelaugustin@gmail.com>
2022-01-09 18:09:17 +0000

    basic server added

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import expres from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = expres();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server running and connected to db at: http://localhost:${PORT}`
+      )
+    )
+  )
+  .catch((err) => console.log(err));
