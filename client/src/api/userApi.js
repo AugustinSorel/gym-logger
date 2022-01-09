5d65686e47a5a50d1e6280f8f88d51a82d8bfcc6
AugustinSorel <sorelaugustin@gmail.com>
2022-01-09 20:54:12 +0000

    api test

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const userApi = axios.create({
-  //   baseURL: "http://localhost:5000/api/user",
-  baseURL: "https://gym-logger-mern.herokuapp.com/api/user",
+  baseURL:
+    process.env.BACKEND_URI + "/api/user" || "http://localhost:5000/api/user",
+  // baseURL: "http://localhost:5000/api/user",
+  // baseURL: "https://gym-logger-mern.herokuapp.com/api/user",
 });
 
 export const addValue = async ({ userId, exerciseId, value }) => {
+  console.log(process.env.BACKEND_URI);
   const res = await userApi.post(`/${userId}/${exerciseId}`, { value });
 
   return res.data;
