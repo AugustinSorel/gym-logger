c377241f9dd11461714e50abc1a3263121259ddb
AugustinSorel <sorelaugustin@gmail.com>
2022-01-10 20:58:48 +0000

    cleaner code

--- client/src/components/PrivateRoute/index.jsx
+++ client/src/components/PrivateRoute/index.jsx
@@ -1,5 +1,4 @@
 import { Navigate } from "react-router-dom";
-import { Welcome } from "../../pages/Welcome";
 import useUser from "../../store/useUser";
 
 export const PrivateRoute = ({ children }) => {
