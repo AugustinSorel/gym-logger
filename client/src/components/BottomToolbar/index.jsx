00283dbe18b54e9815b13856548445b09f3c2d21
AugustinSorel <sorelaugustin@gmail.com>
2022-01-10 15:48:11 +0000

    cleaner code

--- client/src/components/BottomToolbar/index.jsx
+++ client/src/components/BottomToolbar/index.jsx
@@ -1,39 +1,10 @@
-import {
-  whileFocusButton,
-  whileHoverButton,
-  whileTapButton,
-} from "../../framer-motion/whileVariants";
 import * as FooterStyled from "../../styles/BottomToolbar.styled";
 import { DropDownMenu } from "../DropDownMenu";
-import useUser from "../../store/useUser";
 
 export const BottomToolbar = () => {
-  const user = useUser((state) => state.user);
-
   return (
-    <FooterStyled.Container isAuthenticated={user}>
-      {user ? (
-        <DropDownMenu />
-      ) : (
-        <>
-          <FooterStyled.LinkContainer
-            to={"/login"}
-            whileHover={whileHoverButton}
-            whileTap={whileTapButton}
-            whileFocus={whileFocusButton}
-          >
-            Login
-          </FooterStyled.LinkContainer>
-          <FooterStyled.LinkContainer
-            to={"/sign-up"}
-            whileHover={whileHoverButton}
-            whileTap={whileTapButton}
-            whileFocus={whileFocusButton}
-          >
-            Sign Up
-          </FooterStyled.LinkContainer>
-        </>
-      )}
+    <FooterStyled.Container>
+      <DropDownMenu />
     </FooterStyled.Container>
   );
 };
--- client/src/components/GraphSection/index.jsx
+++ client/src/components/GraphSection/index.jsx
@@ -3,31 +3,17 @@ import * as MainStyled from "../../styles/GraphSection.styled";
 import { AddButton } from "../AddButton";
 import { AddValuesModal } from "../AddValuesModal";
 import { AnimatePresence } from "framer-motion";
-import useUser from "../../store/useUser";
 
 export const GraphSection = () => {
   const isOpen = useModal((state) => state.isOpen);
-  const user = useUser((state) => state.user);
-
-  function displayUserNotloggedIn() {
-    return <h1>Please log in to see your data</h1>;
-  }
-
-  function displayGraph() {
-    return (
-      <>
-        <h1>This is the main</h1>
-        <AddButton />
-        <AnimatePresence exitBeforeEnter>
-          {isOpen && <AddValuesModal />}
-        </AnimatePresence>
-      </>
-    );
-  }
 
   return (
     <MainStyled.Container>
-      {user ? displayGraph() : displayUserNotloggedIn()}
+      <h1>This is the main</h1>
+      <AddButton />
+      <AnimatePresence exitBeforeEnter>
+        {isOpen && <AddValuesModal />}
+      </AnimatePresence>
     </MainStyled.Container>
   );
 };
