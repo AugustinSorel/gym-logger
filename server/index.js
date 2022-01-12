edceb419e2d89ce780fef655d54a6058834da1e1
AugustinSorel <sorelaugustin@gmail.com>
2022-01-12 22:51:34 +0000

    cleaner code

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(cors({ origin: true, withCredentials: true }));
 
 mongoose
   .connect(MONGODB_URI)
